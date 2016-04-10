7595dc2def20bb3d78f6588fa618dd20961ad434
FelixSion <pisual@163.com>
2016-04-10 19:54:41 +0800

    edn
    end 结果提交

--- StargazerUIBlackRaven/packageBackUp.java
+++ StargazerUIBlackRaven/packageBackUp.java
@@ -10,13 +10,13 @@ package com.stargazerproject.ui.parameter;
 class StargazerProjectParameter {
 	
 	/**全局根路径**/
-	public static final String BasePath = System.getProperty("user.dir") + "/StargazerUIAssaultLily/";
+	public static final String BasePath = System.getProperty("user.dir") + "/StargazerUIBlackRaven/";
 	
 	/**加载进度页面背景**/
 	public static final String LOADING_INTERFACE_BACKGROUND = BasePath + "Loading.png";
 	
 	/**主界面页面背景**/
-	public static final String MAIN_INTERFACE_BACKGROUND = BasePath + "Background.png";
+	public static final String MAIN_INTERFACE_BACKGROUND = BasePath + "BackGround.png";
 	
 	/**滚动条上部把手背景
 	 *  ▲  ←
@@ -35,25 +35,25 @@ class StargazerProjectParameter {
 	public static final String JScrollPaneUI_Down_PNG = BasePath + "down.png";
 
 	/**滚动条闸道背景颜色 RGB色组**/
-	public static final String JScrollPaneUI_TrackColor = "49,46,63";
+	public static final String JScrollPaneUI_TrackColor = "18,22,34";
 	
 	/**滚动条把手边框颜色 RGB色组**/
-	public static final String JScrollPaneUI_Scroll_Border_Color = "184,140,113";
+	public static final String JScrollPaneUI_Scroll_Border_Color = "34,66,110";
 	
 	/**界面大小**/
-	public static final String Main_Frame_Size = "1800,584";
+	public static final String Main_Frame_Size = "1500,584";
 	
 	/**界面系统图标**/
 	public static final String Main_Frame_IconImage = BasePath + "logo.png";
 	
 	/**操纵图标头像位置及大小**/
-	public static final String Main_Frame_LogoLocation = "1685,20,100,100";
+	public static final String Main_Frame_LogoLocation = "1385,20,100,100";
 	
 	/**操纵图标头像文件路径**/
 	public static final String Main_Frame_Logo_Path = BasePath + "logo.png";
 	
 	/**主界面控制台高度**/
-	public static final String Main_Frame_Console_Height = "584";
+	public static final String Main_Frame_Console_Height = "340";
 	
 	/**控制台备用字体的绝对路径 在系统包含指定字体的情况下将不使用备用字体**/
 	public static final String Main_Frame_Console_StandbyFontPath = BasePath + "Copperplate.ttf";
@@ -62,10 +62,10 @@ class StargazerProjectParameter {
 	public static final String Main_Frame_Console_FontName = "Copperplate";
 	
 	/**控制台指定字体RGB颜色**/
-	public static final String ConsoleText_FontColor = "184,140,113";
+	public static final String ConsoleText_FontColor = "255,255,255";
 	
 	/**加载界面进度条 RGB色组**/
-	public static final String Loading_Frame_ProgressBar_Color = "184,140,113";
+	public static final String Loading_Frame_ProgressBar_Color = "149,148,148";
 	
 	/**加载界面备用字体的绝对路径 在系统包含指定字体的情况下将不使用备用字体**/
 	public static final String Loading_Frame_ProgressInfo_StandbyFontPath = BasePath + "Copperplate.ttf";
@@ -74,6 +74,6 @@ class StargazerProjectParameter {
 	public static final String Loading_Frame_ProgressInfo_FontName = "Copperplate";
 	
 	/**加载界面指定字体颜色**/
-	public static final String loading_ProgressInfo_FontColor = "184,140,113";
+	public static final String loading_ProgressInfo_FontColor = "255,255,255";
 
 }
